1970bc2f38881f084513ab2a7357be34397b4adf
Dmitriy Volkov <d.volkov@email.com>
2020-03-25 15:51:49 +0300

    start 15 lesson

---
+++ src/templates/post-list.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+import Post from '../components/Post'
+
+const PostList = ({ data, pageContext }) => {
+  const posts = data.allMarkdownRemark.edges
+  const { currentPage } = pageContext
+
+  return (
+    <Layout headline={`Page ${currentPage}`}>
+      { posts.map(({ node }) => (
+        <Post 
+          key={node.id}
+          slug={node.fields.slug}
+          title={node.frontmatter.title}
+          author={node.frontmatter.author}
+          date={node.frontmatter.date}
+          body={node.excerpt}
+          tags={node.frontmatter.tags}
+          fluid={node.frontmatter.image.childImageSharp.fluid}
+        />
+      ))}
+    </Layout>
+  )
+}
+
+export const postListQuery = graphql`
+  query postListQuery($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM Do YYYY")
+            author
+            tags
+            image {
+              childImageSharp {
+                fluid(maxWidth: 650, maxHeight: 371) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default PostList
